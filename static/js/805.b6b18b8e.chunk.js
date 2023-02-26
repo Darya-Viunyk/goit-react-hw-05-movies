537dc2e4c62744679773c953903697b966546d24
Darya-Viunyk <105375480+Darya-Viunyk@users.noreply.github.com>
2023-02-26 12:33:13 +0000

    Deploying to gh-pages from @ Darya-Viunyk/goit-react-hw-05-movies@6d1ad50fd4fd42c95f87ac4a17044a4098e8a6c9 🚀

---
+++ static/js/805.b6b18b8e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,t,s){s.r(t);var i=s(184);t.default=function(){return(0,i.jsx)("p",{children:"Sorry, but this page does not exist."})}}}]);
+//# sourceMappingURL=805.b6b18b8e.chunk.js.map
